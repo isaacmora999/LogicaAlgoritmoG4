23987d0478d7e563013d658c7e6cc24530dad884
isaacmora999 <165749027+isaacmora999@users.noreply.github.com>
2024-05-21 09:48:57 -0600

    Add files via upload

---
+++ sesionmay14/funciones.cpp
@@ -0,0 +1,26 @@
+/*funciones*/
+#include <stdio.h>
+#include <stdlib.h>
+/*saludar*/
+void showmessage();
+
+void showmessage(){
+    printf("universidad americana \n");
+    printf("UAM \n");
+
+
+}
+
+void sumarnumeros(int n1, int n2);
+
+void sumarnumeros(int n1, int n2){
+
+int n1=5;
+int n2=4;
+
+int suma=0;
+suma= n1 + n2;
+
+printf("la suma de los numeros es", suma);
+
+}
